fd4db723508aa7c44751c2c38f4d847c8cd34fdf
berberin <souplua@gmail.com>
2023-07-03 16:05:09 +0700

    feat[video player]: set BoxFit.cover

--- lib/src/player_with_controls.dart
+++ lib/src/player_with_controls.dart
@@ -24,9 +24,7 @@ class PlayerWithControls extends StatelessWidget {
       BuildContext context,
       ChewieController chewieController,
     ) {
-      return chewieController.showControls
-          ? chewieController.customControls ?? const AdaptiveControls()
-          : const SizedBox();
+      return chewieController.showControls ? chewieController.customControls ?? const AdaptiveControls() : const SizedBox();
     }
 
     Widget buildPlayerWithControls(
@@ -35,24 +33,30 @@ class PlayerWithControls extends StatelessWidget {
     ) {
       return Stack(
         children: <Widget>[
-          if (chewieController.placeholder != null)
-            chewieController.placeholder!,
+          if (chewieController.placeholder != null) chewieController.placeholder!,
           InteractiveViewer(
             transformationController: chewieController.transformationController,
             maxScale: chewieController.maxScale,
             panEnabled: chewieController.zoomAndPan,
             scaleEnabled: chewieController.zoomAndPan,
             child: Center(
-              child: AspectRatio(
-                aspectRatio: chewieController.aspectRatio ??
-                    chewieController.videoPlayerController.value.aspectRatio,
-                child: VideoPlayer(chewieController.videoPlayerController),
+              child: SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.cover,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: AspectRatio(
+                      aspectRatio: chewieController.aspectRatio ?? chewieController.videoPlayerController.value.aspectRatio,
+                      child: VideoPlayer(chewieController.videoPlayerController),
+                    ),
+                  ),
+                ),
               ),
             ),
           ),
           if (chewieController.overlay != null) chewieController.overlay!,
-          if (Theme.of(context).platform != TargetPlatform.iOS &&
-              !chewieController.disableOverlay)
+          if (Theme.of(context).platform != TargetPlatform.iOS && !chewieController.disableOverlay)
             Consumer<PlayerNotifier>(
               builder: (
                 BuildContext context,
